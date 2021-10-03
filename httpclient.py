b67cc5fc802cbacd37244c20072d9c32412ae0e3
Katherine Richards <kar3@ualberta.ca>
2021-10-03 11:04:18 -0600

    implement requirements

--- httpclient.py
+++ httpclient.py
@@ -45,22 +45,50 @@ class HTTPClient(object):
         else: # hostname and port are present in the request
             return host, port
 
+    # https://docs.python.org/3/library/http.client.html#http.client.HTTPConnection.connect
+    # apparently this can be called automatically 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        # print(data)
+        return int(data.split()[1])
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        return data.split('\r\n\r\n')[0]
+
+    def get_request(self, parsed_url):
+        path = '/' if not parsed_url.path else parsed_url.path # empty string if not present
+        status = f'GET {path} HTTP/1.1\r\n'
+        headers = f'Host: {parsed_url.hostname}\r\nAccept: */*\r\nAccept-Charset: UTF-8\r\nConnection: close\r\n'
+        CRLF = f'\r\n'
+        return status + headers + CRLF
+    
+    def get_post(self, parsed_url, query):
+        path = '/' if not parsed_url.path else parsed_url.path # empty string if not present
+        length = len(query.encode('utf-8')) 
+        status = f'POST {path} HTTP/1.1\r\n'
+        headers = f'Host: {parsed_url.hostname}\r\nAccept: */*\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {length}\r\nConnection: close\r\n'
+        CRLF = f'\r\n'
+        return status + headers + CRLF + f'{query}'
 
     def get_body(self, data):
-        return None
+        return data.split('\r\n\r\n')[1]
+
+    # helper function to print results to stdout
+    def print_response(self, code, headers, body):
+        print(code)
+        print(headers)
+        print(body)
+        return
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
+
+    def parse_encoded_url(self, args):
+        return urllib.parse.urlencode(args) if args != None else urllib.parse.urlencode('')
         
     def close(self):
         self.socket.close()
@@ -78,19 +106,48 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
+        code = 500 # Internal server error :O
         body = ""
-        print("we're supposed to GET stuff here.")
-        parsed_url = urllib.parse(url)
+        # print("we're supposed to GET stuff here.")
+        # parsed_url = urllib.parse.urlparse(url)
 
         # client_host = parsed_url.hostname
-        client_host, client_port = self.get_host_port(parsed_url)
-        return HTTPResponse(code, body)
+        client_host, client_port = self.get_host_port(urllib.parse.urlparse(url))
+        self.connect(client_host, client_port)
+        data = self.get_request(urllib.parse.urlparse(url))
+        self.sendall(data) # send request to the server
+        response = self.recvall(self.socket) # get the response
+        
+        # start constructing the response 
+        code = self.get_code(response)
+        headers = self.get_headers(response)
+        body = self.get_body(response)
+        self.print_response(code, headers, body) # send it!!
+
+        self.close()
+        return HTTPResponse(code, body) # User story: as dev, return HTTPResponse object
 
     def POST(self, url, args=None):
-        code = 500
+        code = 500 # Internal server error :O
         body = ""
-        return HTTPResponse(code, body)
+        # print("POST stuff will go here.")
+        # print(args)
+
+        client_host, client_port = self.get_host_port(urllib.parse.urlparse(url))
+        client_query = self.parse_encoded_url(args)
+        self.connect(client_host, client_port)
+        data = self.get_post(urllib.parse.urlparse(url), client_query)
+        self.sendall(data)
+        response = self.recvall(self.socket)
+        
+        # start constructing the response
+        code = self.get_code(response)
+        headers = self.get_headers(response)
+        body = self.get_body(response)
+        self.print_response(code, headers, body) # send it!!
+
+        self.close()
+        return HTTPResponse(code, body) # User story: as dev, return HTTPResponse object
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
